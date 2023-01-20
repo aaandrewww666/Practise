2ea0d77613cfdcb9d4889030a72a55360170baf8
an <aaandrewww666@mail.ru>
2023-01-20 17:43:30 +0300

    Добавлена функция выбора формата

--- main.cpp
+++ main.cpp
@@ -273,6 +273,16 @@ private:
         }
     }
 
+    VkSurfaceFormatKHR chooseSwapSurfaceFormat(const std::vector<VkSurfaceFormatKHR>& availableFormats) {
+        for (const auto& availableFormat : availableFormats) {
+            if (availableFormat.format == VK_FORMAT_B8G8R8A8_SRGB && availableFormat.colorSpace == VK_COLOR_SPACE_SRGB_NONLINEAR_KHR) {
+                return availableFormat;
+            }
+        }
+
+        return availableFormats[0];
+    }
+
     bool isDeviceSuitable(VkPhysicalDevice device) {
         QueueFamilyIndices indices = findQueueFamilies(device);
 
